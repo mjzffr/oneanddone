8b3f8c4a0299fa046f99ce4cb09a7475592b3985
Maja Frydrychowicz <mjzffr@gmail.com>
2014-07-25 19:49:16 -0400

    Add models for importing from Bugzilla

---
+++ oneanddone/tasks/migrations/0012_auto__add_bugzillabug__add_taskinvalidationcriterion__add_taskimportba.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'BugzillaBug'
+        db.create_table('tasks_bugzillabug', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('summary', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('bugzilla_id', self.gf('django.db.models.fields.IntegerField')(unique=True, max_length=20)),
+        ))
+        db.send_create_signal('tasks', ['BugzillaBug'])
+
+        # Adding model 'TaskInvalidationCriterion'
+        db.create_table('tasks_taskinvalidationcriterion', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('field_name', self.gf('django.db.models.fields.CharField')(max_length=80)),
+            ('field_value', self.gf('django.db.models.fields.CharField')(max_length=80)),
+            ('relation', self.gf('django.db.models.fields.IntegerField')()),
+            ('batch', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['tasks.TaskImportBatch'])),
+        ))
+        db.send_create_signal('tasks', ['TaskInvalidationCriterion'])
+
+        # Adding model 'TaskImportBatch'
+        db.create_table('tasks_taskimportbatch', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('query', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('source', self.gf('django.db.models.fields.IntegerField')(default=0)),
+            ('template_task_data', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('tasks', ['TaskImportBatch'])
+
+
+        # Changing field 'TaskAttempt.user'
+        db.alter_column('tasks_taskattempt', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, on_delete=models.SET_NULL))
+        # Adding field 'Task.content_type'
+        db.add_column('tasks_task', 'content_type',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Task.object_id'
+        db.add_column('tasks_task', 'object_id',
+                      self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Task.batch'
+        db.add_column('tasks_task', 'batch',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['tasks.TaskImportBatch'], null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Task.is_valid'
+        db.add_column('tasks_task', 'is_valid',
+                      self.gf('django.db.models.fields.BooleanField')(default=True),
+                      keep_default=False)
+
+
+        # Changing field 'Task.creator'
+        db.alter_column('tasks_task', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True))
+
+        # Changing field 'TaskKeyword.creator'
+        db.alter_column('tasks_taskkeyword', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True))
+
+        # Changing field 'TaskType.creator'
+        db.alter_column('tasks_tasktype', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True))
+
+        # Changing field 'TaskTeam.creator'
+        db.alter_column('tasks_taskteam', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True))
+
+        # Changing field 'TaskProject.creator'
+        db.alter_column('tasks_taskproject', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True))
+
+    def backwards(self, orm):
+        # Deleting model 'BugzillaBug'
+        db.delete_table('tasks_bugzillabug')
+
+        # Deleting model 'TaskInvalidationCriterion'
+        db.delete_table('tasks_taskinvalidationcriterion')
+
+        # Deleting model 'TaskImportBatch'
+        db.delete_table('tasks_taskimportbatch')
+
+
+        # Changing field 'TaskAttempt.user'
+        db.alter_column('tasks_taskattempt', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['auth.User']))
+        # Deleting field 'Task.content_type'
+        db.delete_column('tasks_task', 'content_type_id')
+
+        # Deleting field 'Task.object_id'
+        db.delete_column('tasks_task', 'object_id')
+
+        # Deleting field 'Task.batch'
+        db.delete_column('tasks_task', 'batch_id')
+
+        # Deleting field 'Task.is_valid'
+        db.delete_column('tasks_task', 'is_valid')
+
+
+        # Changing field 'Task.creator'
+        db.alter_column('tasks_task', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['auth.User']))
+
+        # Changing field 'TaskKeyword.creator'
+        db.alter_column('tasks_taskkeyword', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['auth.User']))
+
+        # Changing field 'TaskType.creator'
+        db.alter_column('tasks_tasktype', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['auth.User']))
+
+        # Changing field 'TaskTeam.creator'
+        db.alter_column('tasks_taskteam', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['auth.User']))
+
+        # Changing field 'TaskProject.creator'
+        db.alter_column('tasks_taskproject', 'creator_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['auth.User']))
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'tasks.bugzillabug': {
+            'Meta': {'object_name': 'BugzillaBug'},
+            'bugzilla_id': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'max_length': '20'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'summary': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'tasks.feedback': {
+            'Meta': {'object_name': 'Feedback'},
+            'attempt': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tasks.TaskAttempt']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        'tasks.task': {
+            'Meta': {'object_name': 'Task'},
+            'batch': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tasks.TaskImportBatch']", 'null': 'True', 'blank': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'difficulty': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'execution_time': ('django.db.models.fields.IntegerField', [], {'default': '15'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'instructions': ('django.db.models.fields.TextField', [], {}),
+            'is_draft': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_valid': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'prerequisites': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tasks.TaskProject']", 'null': 'True', 'blank': 'True'}),
+            'repeatable': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'short_description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'start_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tasks.TaskTeam']"}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tasks.TaskType']", 'null': 'True', 'blank': 'True'}),
+            'why_this_matters': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        'tasks.taskattempt': {
+            'Meta': {'ordering': "['-modified']", 'object_name': 'TaskAttempt'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'state': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'taskattempt_set'", 'to': "orm['tasks.Task']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'on_delete': 'models.SET_NULL'})
+        },
+        'tasks.taskimportbatch': {
+            'Meta': {'object_name': 'TaskImportBatch'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'query': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'source': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'template_task_data': ('django.db.models.fields.TextField', [], {})
+        },
+        'tasks.taskinvalidationcriterion': {
+            'Meta': {'object_name': 'TaskInvalidationCriterion'},
+            'batch': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['tasks.TaskImportBatch']"}),
+            'field_name': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'field_value': ('django.db.models.fields.CharField', [], {'max_length': '80'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'relation': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'tasks.taskkeyword': {
+            'Meta': {'object_name': 'TaskKeyword'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'keyword_set'", 'to': "orm['tasks.Task']"})
+        },
+        'tasks.taskproject': {
+            'Meta': {'object_name': 'TaskProject'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'tasks.taskteam': {
+            'Meta': {'object_name': 'TaskTeam'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'tasks.tasktype': {
+            'Meta': {'object_name': 'TaskType'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['tasks']
--- oneanddone/tasks/models.py
+++ oneanddone/tasks/models.py
@@ -3,6 +3,8 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models import Q
@@ -13,6 +15,53 @@ import jinja2
 from markdown import markdown
 
 from oneanddone.base.models import CachedModel, CreatedByModel, CreatedModifiedModel
+
+
+class TaskInvalidationCriterion(models.Model):
+    NOT_EQUAL = 0
+    EQUAL = 1
+    field_name = models.CharField(max_length=80)
+    field_value = models.CharField(max_length=80)
+    relation = models.IntegerField(choices=(
+                (EQUAL, '=='),
+                (NOT_EQUAL, '!=')
+               ))
+    batch = models.ForeignKey('TaskImportBatch')
+
+    def __unicode__(self):
+        return ''.join([self.field_name, self.relation, self.field_value])
+
+
+class TaskImportBatch(CreatedModifiedModel, CreatedByModel):
+    name = models.CharField(max_length=255)
+    query = models.CharField(max_length=255)
+    # other sources might be Moztrap, etc.
+    BUGZILLA = 0
+    OTHER = 1
+    source = models.IntegerField(
+        choices=(
+            (BUGZILLA, 'Bugzilla@Mozilla'),
+            (OTHER, 'Other')
+        ),
+        default=BUGZILLA)
+    # (?) JSON-encoded results from the batch creation form
+    template_task_data = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+    query.help_text = """
+        The URL to the search query that yields the items you want to
+        create tasks from.
+    """
+
+
+class BugzillaBug(models.Model):
+    summary = models.CharField(max_length=255)
+    bugzilla_id = models.IntegerField(max_length=20, unique=True)
+
+    def __unicode__(self):
+        return ' '.join(['Bug', self.bugzilla_id])
 
 
 class TaskProject(CachedModel, CreatedModifiedModel, CreatedByModel):
@@ -45,6 +94,15 @@ class Task(CachedModel, CreatedModifiedModel, CreatedByModel):
     team = models.ForeignKey(TaskTeam)
     type = models.ForeignKey(TaskType, blank=True, null=True)
 
+    # imported_item may be BugzillaBug for now. In future, other sources such
+    # as Moztrap may be possible
+    content_type = models.ForeignKey(ContentType, null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    imported_item = generic.GenericForeignKey('content_type', 'object_id')
+
+    # batch that created this Task
+    batch = models.ForeignKey(TaskImportBatch, blank=True, null=True)
+
     EASY = 1
     INTERMEDIATE = 2
     ADVANCED = 3
@@ -56,7 +114,6 @@ class Task(CachedModel, CreatedModifiedModel, CreatedByModel):
         ),
         default=EASY,
         verbose_name='task difficulty')
-    end_date = models.DateTimeField(blank=True, null=True)
     execution_time = models.IntegerField(
         choices=((i, i) for i in (15, 30, 45, 60)),
         blank=False,
@@ -65,11 +122,13 @@ class Task(CachedModel, CreatedModifiedModel, CreatedByModel):
     )
     instructions = models.TextField()
     is_draft = models.BooleanField(verbose_name='draft?')
+    is_valid = models.BooleanField(verbose_name='valid?', default=True)
     name = models.CharField(max_length=255, verbose_name='title')
     prerequisites = models.TextField(blank=True)
     repeatable = models.BooleanField(default=True)
     short_description = models.CharField(max_length=255, verbose_name='description')
     start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
     why_this_matters = models.TextField(blank=True)
 
     def _yield_html(self, field):
