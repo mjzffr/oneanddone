e3234b5cafc3f7a756f5d22db851287cc8d4722a
Maja Frydrychowicz <mjzffr@gmail.com>
2014-07-30 15:52:18 -0400

    Add wrapper around BMO REST API

---
+++ oneanddone/tasks/bugzilla.py
@@ -0,0 +1,43 @@
+import requests
+
+_baseurl = 'https://bugzilla.mozilla.org/rest/bug'
+
+def _request_json(url, params):
+    """ Returns the json-encoded response from Bugzilla@Mozilla, if any """
+    headers = {'content-type':'application/json', 'accept':'application/json'}
+    try:
+        r = requests.get(url, headers=headers, params=params)
+        data = r.json()
+        if r.status_code != 200 or data.get('error'):
+            data = {}
+            # TODO mzf log the error somewhere?
+        return data
+    except requests.exceptions.RequestException:
+        # TODO mzf log the exception somewhere?
+        return {}
+
+def request_bugs(query, fields=['id','summary']):
+    """ Returns list of bugs from Bugzilla@Mozilla, if any """
+    params = {'include_fields' : ','.join(fields)}
+    url = ''.join([_baseurl, '?', query])
+    return _get_json(url, params).get('bugs') or []
+
+def request_bug(bug_id, fields=['id','summary']):
+    """ Returns bug with id `bug_id` from Buzgilla@Mozilla, if any """
+    params = {'include_fields' : ','.join(fields)}
+    url = ''.join([_baseurl, '/', str(bug_id)])
+    bugs = _get_json(url, params).get('bugs')
+    if bugs:
+        return bugs[0]
+    else:
+        return None
+
+
+
+
+
+
+
+
+
+
